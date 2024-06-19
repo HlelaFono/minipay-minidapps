9606721f6e443449a5ba377b9e248714f691a8f5
HlelaFono <fonohlela@gmail.com>
2024-06-19 15:10:41 +0000

    minipayDApp allow user to send money to another crypto wallet

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
-import PayTokenFunctionCall from "./dApps/PayTokenFunctionCall";
 import TransferCUSD from "./dApps/TransferCUSD";
 
 export default function App() {
     return (
         <div
             style={{
-                marginTop: "20px",
+                marginTop: "10px",
                 display: "flex",
                 flexDirection: "column",
                 alignItems: "center",
-                gap: "12px",
+                gap: "9px",
+                backgroundColor: "lightblue",
+                padding: "20px",
+                borderRadius: "20px",
+                background: "linear-gradient(180deg, #116C31, #71F53E, #116C31)"
             }}
-        >
+            >
             <TransferCUSD />
-            <PayTokenFunctionCall />
+           
         </div>
     );
 }
--- src/dApps/TransferCUSD.tsx
+++ src/dApps/TransferCUSD.tsx
@@ -1,48 +1,96 @@
-import { utils } from "ethers";
+import { useState } from "react";
+import { ethers } from "ethers";
+
 
 // Mainnet address of cUSD
 const CUSD_ADDRESS = "0x765DE816845861e75A25fCA122bb6898B8B1282a";
 
-const receiverAddress = "";
 
 // DApp to quickly test transfer of cUSD to a specific address using the cUSD contract.
 export default function TransferCUSD() {
-    async function transferCUSD() {
-        if (window.ethereum) {
-            // Get connected accounts, if not connected request connnection.
-            // returns an array of accounts
-            let accounts = await window.ethereum.request({
-                method: "eth_requestAccounts",
-            });
+    const [receiverAddress, setReceiverAddress] = useState("");
+    const [userAddress, setUserAddress] = useState("");
+    const [transactionStatues, setTransactionStatues] = useState("");
+    const [isHovered, setIsHovered] = useState(false);
 
-            // The current selected account out of the connected accounts.
-            let userAddress = accounts[0];
+    const  transferCUSD = async () => {
+        try {
+            const { ethereum } = window;
+            if (!ethereum) throw new Error("MetaMusk not detected, install MetaMusk");
+            
+            const accounts = await ethereum.request({method: "eth_requestAccounts"});
+            setUserAddress(accounts[0]);
 
-            let iface = new utils.Interface([
-                "function transfer(address to, uint256 value)",
-            ]);
+            const provider = new ethers.providers.Web3Provider(ethereum);
+            const signer = provider.getSigner();
 
-            let calldata = iface.encodeFunctionData("transfer", [
-                receiverAddress,
-                utils.parseEther("0.1"), // 10 cUSD - This amount is in wei
-            ]);
+            const cUSDContract = new ethers.Contract(CUSD_ADDRESS, ["function transfer(address to, uint256 value)"], signer);
+            
+            const tx = await cUSDContract.transfer(receiverAddress, ethers.utils.parseEther("0.1"));
+            await provider.waitForTransaction(tx.hash);
 
-            // Send transaction to the injected wallet to be confirmed by the user.
-            let tx = await window.ethereum.request({
-                method: "eth_sendTransaction",
-                params: [
-                    {
-                        from: userAddress,
-                        to: CUSD_ADDRESS, // We need to call the transfer function on the cUSD token contract
-                        data: calldata, // Information about which function to call and what values to pass as parameters
-                    },
-                ],
-            });
-
-            // Wait until tx confirmation and get tx receipt
-            let receipt = await tx.wait();
+            setTransactionStatues("Transaction successfull !!");
+        } catch (error) {
+            if(error instanceof Error) {
+                console.error("Error tranferring cUSD:", error);
+                setTransactionStatues(`Error: ${error.message}`);
+            }
+            
         }
-    }
+    };
+    return (
+        <div
+        style = {
+            {
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                backgroundColor: "lightgreen",
+                height: "90vh",
+                width: "100px",
+                justifyContent: "center",
+                borderRadius: "10px"
+               
+                
+            }
+        }
+        >
+            {
+                userAddress && <p> Your Address: {userAddress}</p>
+            }
+            <input
+                type="text"
+                value={receiverAddress}
+                onChange={(e) => setReceiverAddress(e.target.value)}
+                placeholder="Receiver's Address"
+                style={{
+                    marginBottom: "10px",
+                    borderRadius: "20px",
+                    padding: "10px",
+                    width: "300px",
+                    border: "3px solid rgb(0,127,0)"
+                }}
+            ></input>
+            <button
+            onClick={transferCUSD}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+            style={{
+                marginBottom: "10px",
+                borderRadius: "20px",
+                border: "2px solid rgb(0,127,0)",
+                color: "rgb(255,255,255)",
+                backgroundColor: isHovered ? "darkgreen" : "green",
+                padding: "10px 20px",
+                cursor: "pointer"
 
-    return <button onClick={transferCUSD}>Transfer cUSD</button>;
+            }}
+            >
+                Transfer cUSD
+            </button>
+            {transactionStatues && <p>{transactionStatues}</p>}
+        </div>
+
+
+    );
 }
